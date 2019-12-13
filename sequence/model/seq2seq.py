fix bug with softmax

sequence/model/seq2seq.py
@@ -69,7 +69,7 @@ class EncoderDecoder(nn.Module):
             num_layers=rnn_layers,
         )
         self.decoder_out = nn.Sequential(
-            nn.Linear(self.linear_in, vocabulary_size), nn.LogSoftmax(1)
+            nn.Linear(self.linear_in, vocabulary_size), nn.LogSoftmax(-1)
         )
 
     def apply_emb(self, x):

sequence/test/test_vae.py
@@ -1,13 +1,17 @@
-from sequence.model.vae import VAE, det_neg_elbo
+import torch
+import numpy as np
+from sequence.model.vae import VAE, det_neg_elbo, run_decoder
 from sequence.test import language, words, dataset, paths
 
 
 def test_flow(language, dataset):
     m = VAE(language.vocabulary_size)
     batch_size = 3
-
-    packed_padded, padded = dataset.get_batch(0, batch_size)
-    h, z, mu, log_var = m.encode(packed_padded)
+    with torch.no_grad():
+        packed_padded, padded = dataset.get_batch(0, batch_size)
+        h, z, mu, log_var = m.encode(packed_padded)
+        out, target = run_decoder(m, packed_padded, word_dropout=1, h=h)
+        assert np.all(np.isclose(out.exp().sum(-1), 1))
 
 
 def test_loss(language, dataset):