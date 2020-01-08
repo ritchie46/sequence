4bf0bdc1020258733d214ab4ab3196c9ee394685
ritchie46 <ritchie46@gmail.com>
2020-01-08 08:52:48 +0100

    init p@k

--- sequence/metrics.py
+++ sequence/metrics.py
@@ -2,11 +2,31 @@ import numpy as np
 
 
 def p_at_k(pred, target, k=20):
-    target = target.cpu().data.numpy().T
+    """
+
+    Parameters
+    ----------
+    pred : Union[torch.Tensor, np.array]
+        Shape: (seq_len, batch)
+    target : Union[torch.Tensor, np.array]
+        Shape: (seq_len, batch)
+    k : int
+
+    Returns
+    -------
+
+    """
+    if hasattr(target, 'cpu'):
+        target = target.cpu().data.numpy().T
+    else:
+        target = target.T
+
+    if hasattr(target, 'cpu'):
+        pred = pred.cpu().data.numpy()
 
     # shape: (batch, k)
     # numpys sort is faster
-    pred_top = np.argsort(axis=0, a=pred.cpu().data.numpy())[::-1][:k, ...].squeeze().T
+    pred_top = np.argsort(axis=0, a=pred)[::-1][:k, ...].squeeze().T
     # EOS to a non existing index
     pred_top[pred_top == 0] = -999
 
