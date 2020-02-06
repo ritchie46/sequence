68f5753b02b5a694497f9ccce40dce831116d2b6
ritchie46 <ritchie46@gmail.com>
2020-02-06 10:31:29 +0100

    pass name to MR in test

--- sequence/test/test_callbacks.py
+++ sequence/test/test_callbacks.py
@@ -3,7 +3,7 @@ from dumpster.registries.file import ModelRegistry
 
 
 def test_register_global_step():
-    mr = ModelRegistry()
+    mr = ModelRegistry("test")
     f = callbacks.register_global_step(mr)
     f(global_step=13,)
     assert mr.global_step_ == 13
