00fb6482c331879424bac58e8cb361bf39d90ff1
Rick Bruins <rick.bruins@xomnia.com>
2020-08-26 15:39:24 +0200

    range that is used for validating the model during training time

--- sequence/main/stamp.py
+++ sequence/main/stamp.py
@@ -36,7 +36,7 @@ def main(args: argparse.Namespace):
     callbacks_ = generic.init_callbacks(args, model_registry, artifact_dir)
     global_step = generic.init_global_step(args, model_registry)
     device = generic.init_device(args, model_registry)
-    callbacks_.append(log_ranking_metrics(50, k=args.top_k))
+    callbacks_.append(log_ranking_metrics(n=args.n_log_range, k=args.top_k))
 
     for e in range(args.epochs):
         global_step = run_epoch(
